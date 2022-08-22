3e8bcc9011b975e3ae8ed15f4775e7c83245bc1f
Prithwis Das <prithwisdas2017@gmail.com>
2022-08-21 19:57:39 -0700

    Add files via upload

---
+++ Pandemic Simulation/main.py
@@ -0,0 +1,33 @@
+import numpy as np
+from sklearn import svm
+from sklearn.svm import SVR
+from pandas import read_csv
+import matplotlib.pyplot as plt
+import random
+from random import randrange
+
+years = 0
+n_epochs = 100000
+res1 = []
+
+for i in range(n_epochs):
+  year = 0
+  while True:
+    rand = randrange(100000)
+    if rand<5768:
+      res1.append(year)
+      break
+    else:
+      years += 1
+      year += 1
+
+print(years/n_epochs)
+
+plt.rc('font', weight='bold')
+plt.rcParams.update({'font.size': 22})
+plt.rcParams["figure.figsize"] = (15,15)
+plt.hist(res1, bins=20, range = (0,800))
+plt.title('Pandemic Simulation Distribution', fontweight="bold")
+plt.ylabel('Number of Simulations', fontweight="bold")
+plt.xlabel('Years Survived', fontweight="bold")
+plt.savefig('pandemic.png', dpi=500)
