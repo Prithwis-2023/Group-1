6f1f308c0bc7168e77ab761b4297b4e5ef497f7d
Prithwis Das <prithwisdas2017@gmail.com>
2022-08-24 23:56:24 +0000

    updation

--- Artificial Intelligence SImulation/main.py
+++ Artificial Intelligence SImulation/main.py
@@ -17,12 +17,14 @@ for elements in df["FLOPS"]:
     fl.append(elements)
 for elements in df["Year"]:
     ye.append(elements)
+
+plt.rc('font', weight='bold')
 plt.rcParams.update({'font.size': 22})    
 plt.rcParams['figure.figsize'] = (15,13)
 
-plt.xlabel("Years")
-plt.ylabel("Floating Point Operations per Second")
-plt.title("FLOPS of Supercomputers from 1993-2021")
+plt.xlabel("Years", fontweight='bold')
+plt.ylabel("Floating Point Operations per Second", fontweight='bold')
+plt.title("FLOPS of Supercomputers from 1993-2021", fontweight='bold')
 plt.plot(ye,fl)
 plt.savefig('flops.png', dpi=500)
 
@@ -31,9 +33,9 @@ flops_log = [] #y
 for elements in df["FLOPS"]: 
     flops_log.append(math.log(elements))
 
-plt.xlabel("Years")
-plt.ylabel("Log Base 10 of FLOPS")
-plt.title("Log Base 10 of FLOPS of Supercomputers from 1993-2021")
+plt.xlabel("Years", fontweight='bold')
+plt.ylabel("Log Base 10 of FLOPS", fontweight='bold')
+plt.title("Log Base 10 of FLOPS of Supercomputers from 1993-2021", fontweight='bold')
 plt.scatter(ye,flops_log)
 plt.savefig('log_flops.png', dpi=500)
 
@@ -58,16 +60,16 @@ print(results)
 
 plt.scatter(X,y)
 plt.plot(years_want_to_predict,results, color="r")
-plt.xlabel("Years")
-plt.ylabel("Log Base 10 of FLOPS of Supercomputers")
-plt.title("Predictions on FLOPS using Linear Regression")
+plt.xlabel("Years", fontweight='bold')
+plt.ylabel("Log Base 10 of FLOPS of Supercomputers", fontweight='bold')
+plt.title("Predictions on FLOPS using Linear Regression", fontweight='bold')
 plt.savefig('pred.png', dpi=500)
 
 #for HPL-AI
 x = ["Nov 2019", "June 2020", "Nov 2020", "June 2021", "Nov 2021"]
 y = [0.45*10**18, 1.42*10**18, 2.0*10**18, 2.0*10**18, 2.000*10**18]
 plt.plot(x,y)
-plt.xlabel("Time")
-plt.ylabel("HPL-AI readings (Eflop/s)")
-plt.title("Readings based on HPL-AI benchmark 2019-2021")
+plt.xlabel("Time", fontweight='bold')
+plt.ylabel("HPL-AI readings (Eflop/s)", fontweight='bold')
+plt.title("Readings based on HPL-AI benchmark 2019-2021", fontweight='bold')
 plt.savefig('hpl-ai.png', dpi=500)
