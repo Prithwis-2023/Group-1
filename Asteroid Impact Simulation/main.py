913810f3da3e98c07547f7a4be5062ebea3a6efc
Prithwis Das <prithwisdas2017@gmail.com>
2022-08-25 00:28:23 +0000

    update

--- Asteroid Impact Simulation/main.py
+++ Asteroid Impact Simulation/main.py
@@ -13,16 +13,25 @@ df2 = df[(df['Estimated Diameter (km)'] > 0.14) & (df['H (mag)'] <= 22)]
 
 dia = []
 pal = []
-for diam in df2["Estimated Diameter (km)"]:
+dia2 = []
+pal2 = []
+
+for diam in df2["Estimated Diameter (km)"][2:]:
     dia.append(diam)
-for palermo in df2["Palermo Scale (max.)"]:
+for palermo in df2["Palermo Scale (max.)"][2:]:
     pal.append(palermo)
+for element in df2["Estimated Diameter (km)"][:2]:
+    dia2.append(element)
+for element in df2["Palermo Scale (max.)"][:2]:
+    pal2.append(element)    
 plt.rcParams.update({'font.size': 22})
 plt.rcParams["figure.figsize"] = (12,10)
+plt.rc('font', weight='bold')
 plt.scatter(pal, dia, s=500)
-plt.title('Potentialy Hazardous Asteroids')
-plt.xlabel("Palermo Scale (max.)")
-plt.ylabel("Estimated Diameter (km)")
+plt.scatter(pal2, dia2, s=500, color="red")
+plt.title('Potentialy Hazardous Asteroids', fontweight='bold')
+plt.xlabel("Palermo Scale (max.)", fontweight='bold')
+plt.ylabel("Estimated Diameter (km)", fontweight='bold')
 plt.savefig('asteroid_data.png', dpi=500)
 
 impact_probs = []
